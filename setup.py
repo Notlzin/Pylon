179396a7514a4300111ee9101de7b835d4e2fc31
Notlzin <ne1.t3ln@gmail.com>
2025-10-15 15:25:10 +0800

    added and changed parts of setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
+# oh yeah this was made by GPT-5mini, so uhh excuse me #
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 import os
 
-here = os.path.dirname(__file__)  # outer root folder
-package_dir = os.path.join(here, "pylon2d")  # inner package folder
+here = os.path.dirname(__file__)  # outer root folder #
+package_dir = os.path.join(here, "pylon2d")  # inner package folder #
 
 ext_modules = cythonize(
     Extension(
-        "pylon2d.Physics.PhysicsSys",  # Python module path
-        [os.path.join(package_dir, "Physics", "PhysicsSys.pyx")],  # actual file path
+        "pylon2d.Physics.PhysicsSys",  # python module path #
+        [os.path.join(package_dir, "Physics", "PhysicsSys.pyx")],  # actual file path #
     ),
     language_level="3",
 )
 
 setup(
     name="pylon2d",
-    packages=["pylon2d", "pylon2d.Physics"],  # explicitly list packages
+    packages=["pylon2d", "pylon2d.Physics"],  # explicitly list packages #
     ext_modules=ext_modules,
 )
